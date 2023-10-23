9a33c9159bd33e44a1b4612eb97597fb1058003e
killgriff22 <kilgrif22033@gmail.com>
2023-10-23 11:31:46 -0400

    Inital Commit

---
+++ Server.py
@@ -0,0 +1,11 @@
+import flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+flask.run("0.0.0.0","80")
